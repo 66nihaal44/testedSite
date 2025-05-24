e5a594950aedda8d6747a15bd32e4b63e0ba568d
66nihaal44 <nihaalshetty171@gmail.com>
2025-05-23 22:04:58 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-document.getElementByClass('menu').innerHTML = "
+document.getElementByClass("menu").innerHTML = "
   <a href="https://66nihaal44.github.io/testedSite/">Main Page</a>
   <a href="https://66nihaal44.github.io/testedSite/secondpage">Second Page</a>
   <a href="https://66nihaal44.github.io/testedSite/quiz">Quiz</a>
