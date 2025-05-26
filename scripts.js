9fe84a8602b46800b5a5c3fdd6ec1293f0f46e9f
66nihaal44 <nihaalshetty171@gmail.com>
2025-05-25 20:15:13 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,19 +8,19 @@ function addMenu(){
   const mlink1 = document.createElement('a');
   disText = document.createTextNode("Main Page");
   mlink1.appendChild(disText);
-  // mlink1.title = "Main Page";
+  mlink1.title = "Main Page";
   mlink1.href = "";
   document.getElementById("menu").appendChild(mlink1);
   const mlink2 = document.createElement('a');
   disText = document.createTextNode("Fibonacci");
   mlink2.appendChild(disText);
-  // mlink2.title = "Fibonacci";
+  mlink2.title = "Fibonacci";
   mlink2.href = "fibonacci";
   document.getElementById("menu").appendChild(mlink2);
   const mlink3 = document.createElement('a');
   disText = document.createTextNode("Quiz");
   mlink3.appendChild(disText);
-  // mlink3.title = "Quiz";
+  mlink3.title = "Quiz";
   mlink3.href = "quiz";
   document.getElementById("menu").appendChild(mlink3);
 }
