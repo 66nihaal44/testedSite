b2d07cea8080d255b9d2a5859e542d614d3e81e0
66nihaal44 <nihaalshetty171@gmail.com>
2025-05-25 21:13:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,25 +5,19 @@ function changeTextColor(textId, color) {
   document.getElementById(textId).style.color = color;
 }
 function addMenu(){
-  const mlink1 = document.createElement('a');
-  // disText = document.createTextNode("Main Page");
-  // mlink1.appendChild(disText);
-  mlink1.textContent = "Main Page";
-  mlink1.title = "Main Page";
-  mlink1.href = "";
-  document.getElementsByClassName("menu")[0].appendChild(mlink1);
-  const mlink2 = document.createElement('a');
-  // disText = document.createTextNode("Fibonacci");
-  // mlink2.appendChild(disText);
-  mlink2.textContent = "Fibonacci";
-  mlink2.title = "Fibonacci";
-  mlink2.href = "fibonacci";
-  document.getElementsByClassName("menu")[0].appendChild(mlink2);
-  const mlink3 = document.createElement('a');
-  // disText = document.createTextNode("Quiz");
-  // mlink3.appendChild(disText);
-  mlink3.textContent = "Quiz";
-  mlink3.title = "Quiz";
-  mlink3.href = "quiz";
-  document.getElementsByClassName("menu")[0].appendChild(mlink3);
+  mlink = document.createElement('a');
+  mlink.textContent = "Main Page";
+  mlink.title = "Main Page";
+  mlink.href = "";
+  document.getElementsByClassName("menu")[0].appendChild(mlink);
+  // const mlink2 = document.createElement('a');
+  mlink.textContent = "Fibonacci";
+  mlink.title = "Fibonacci";
+  mlink.href = "fibonacci";
+  document.getElementsByClassName("menu")[0].appendChild(mlink);
+  // const mlink3 = document.createElement('a');
+  mlink.textContent = "Quiz";
+  mlink.title = "Quiz";
+  mlink.href = "quiz";
+  document.getElementsByClassName("menu")[0].appendChild(mlink);
 }
